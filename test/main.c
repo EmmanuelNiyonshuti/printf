25dd609fe901023cc7e15d3da5ff0873a3f86778
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-14 11:46:31 -0700

    bug-0_checker issues

--- test/main.c
+++ test/main.c
@@ -9,8 +9,8 @@
  */
 int main(void)
 {
-	char len;
-	char len2;
+	int len;
+	int len2;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
